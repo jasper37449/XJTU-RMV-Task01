21e53dc867e18998a5d515e2e0f45d77939856c0
Jasper Tan <108564332+jasper37449@users.noreply.github.com>
2025-09-15 00:49:02 +0800

    Add files via upload

---
+++ XJTU-RMV-Task01/build/CMakeFiles/testproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testproject.dir/main.cpp.o"
+  "CMakeFiles/testproject.dir/main.cpp.o.d"
+  "testproject"
+  "testproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
